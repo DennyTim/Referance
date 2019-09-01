202ef7c62125f203245f0a2075963211c70db93d
Tim <luckystudydanit@gmail.com>
2019-09-02 02:05:43 +0300

    Added observer features

--- RxJs/reactive-patterns-course/src/app/browser-event-experiments/browser-event-experiments.component.ts
+++ RxJs/reactive-patterns-course/src/app/browser-event-experiments/browser-event-experiments.component.ts
@@ -6,7 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./browser-event-experiments.component.css']
 })
 export class BrowserEventExperimentsComponent implements OnInit {
+  hoverSection: HTMLElement;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.hoverSection = document.getElementById('hover');
+    this.hoverSection.addEventListener('mousemove', onMouseMove);
+    this.hoverSection.addEventListener('click', onMouseMove);
+  }
+
+  unsubscribe() {
+    console.log('Called unsubscribe...');
+
+    this.hoverSection.removeEventListener('mousemove', onMouseMove);
+  }
+}
+
+function onClick(ev: Event) {
+  console.log('click', ev);
+}
+
+function onMouseMove(ev: MouseEvent) {
+  console.log(ev);
 }
---
+++ RxJs/reactive-patterns-course/src/app/shared/model/test-lessons.ts
@@ -0,0 +1,19 @@
+import { Lesson } from './lesson';
+
+export const testLessons: Lesson[] = [
+  {
+    id: 1,
+    description: 'Setting Up an Angular Development',
+    duration: '5:00'
+  },
+  {
+    id: 2,
+    description: 'Running the Lessons Code',
+    duration: '7:00'
+  },
+  {
+    id: 3,
+    description: 'Build Your First App',
+    duration: '9:00'
+  }
+];
