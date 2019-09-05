4b017ce55d9dd9e2b19ca719f69f6162aa316cc8
Tim <luckystudydanit@gmail.com>
2019-09-05 08:06:11 +0300

    Added changes to RxJS

--- RxJs/reactive-patterns-course/src/app/lessons-list/lessons-list.component.ts
+++ RxJs/reactive-patterns-course/src/app/lessons-list/lessons-list.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  globalEventBus,
-  Observer,
-  LESSONS_LIST_AVAILABLE,
-  ADD_NEW_LESSON
-} from '../event-bus-experiments/event-bus';
+import { Observer, store } from '../event-bus-experiments/app-data';
 import { Lesson } from '../shared/model/lesson';
 import * as _ from 'lodash';
 
@@ -13,36 +8,27 @@ import * as _ from 'lodash';
   templateUrl: './lessons-list.component.html',
   styleUrls: ['./lessons-list.component.css']
 })
-export class LessonsListComponent implements OnInit, Observer {
+export class LessonsListComponent implements Observer, OnInit {
   lessons: null | Lesson[] = null;
 
   constructor() {
     console.log('lesson list component is registered as observer');
-    globalEventBus.registerObserver(LESSONS_LIST_AVAILABLE, this);
-
-    globalEventBus.registerObserver(ADD_NEW_LESSON, {
-      notify: lessonText => {
-        this.lessons.push({
-          id: Math.random(),
-          description: lessonText
-        });
-      }
-    });
   }
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    store.lessonsList$.subscribe(this);
+  }
 
-  notify(data: any) {
+  next(data: any) {
     console.log('Lessons list component received data');
-    this.lessons = data.slice(0);
+    this.lessons = data;
   }
 
   toggleLessonViewed(lesson: Lesson) {
-    console.log('Toggling lesson...');
-    lesson.completed = !lesson.completed;
+    store.toggleLessonViewed(lesson);
   }
 
   delete(deleted: Lesson): void {
-    _.remove(this.lessons, lesson => lesson.id === deleted.id);
+    store.deleteLesson(deleted);
   }
 }
